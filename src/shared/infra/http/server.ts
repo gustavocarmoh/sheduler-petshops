098fe65be7d7a765861936159ef36f885e40f39f
Gustavo do Carmo Hermenegildo <gustavo_hermenegildo@hotmail.com>
2021-01-15 16:05:01 -0300

    [WIP]

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,22 +1,23 @@
-import express, { NextFunction , Request, Response} from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import 'reflect-metadata';
-import routes from './routes';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import routes from './routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
-var helmet = require('helmet')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const helmet = require('helmet');
 
 app.disabled('x-powered-by');
 
 app.use(helmet);
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -24,7 +25,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     return response.status(err.statusCode).json({
       status: 'error',
       message: err.message,
-    })
+    });
   }
 
   console.error(err);
@@ -32,7 +33,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   return response.status(500).json({
     status: 'error',
     message: 'internal server error',
-  })
+  });
 });
 
 app.listen(3333, () => {
